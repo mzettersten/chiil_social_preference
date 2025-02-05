30fe1f87f2d1ea817f46a4bb89e8d5ed73837999
mzettersten <mzettersten@users.noreply.github.com>
2025-02-05 14:34:04 -0800

    fix preloading

--- experiment/js/trials.js
+++ experiment/js/trials.js
@@ -1,32 +1,29 @@
 var practice_trials = [
   {
-    "pair": "practice_1",
-    "image_1": "106_f",
-    "image_2": "348_m",
-    "condition": "gender",
-    "genders": "m-f",
-    "interest_condition_l": "hi",
-    "interest_condition_r": "li"
+    "image_1": "249_m",
+    "image_2": "315_m",
+    "condition": "matched",
+    "genders": "m-m",
+    "interest_l": "Circles",
+    "interest_r": "Squares"
   },
   {
-    "pair": "practice_2",
-    "image_1": "88_f",
-    "image_2": "364_m",
-    "condition": "gender",
+    "image_1": "48_m",
+    "image_2": "232_f",
     "genders": "m-f",
-    "interest_condition_l": "hi",
-    "interest_condition_r": "li"
+    "condition": "gender",
+    "interest_l": "Squares",
+    "interest_r": "Circles"
   },
   {
-    "pair": "practice_3",
-    "image_1": "274_f",
-    "image_2": "372_m",
-    "condition": "gender",
-    "genders": "m-f",
-    "interest_condition_l": "li",
-    "interest_condition_r": "hi"
+    "image_1": "226_f",
+    "image_2": "363_f",
+    "genders": "f-f",
+    "condition": "matched",
+    "interest_l": "Circles",
+    "interest_r": "Squares"
   }
-]
+];
 
 
 var social_pref_trials = [
